4808fa0cd3f5a632c788c983bca485f6e765133d
tehnarter <tehnarter@gmail.com>
2026-01-15 12:27:43 +0200

    added components Product/AddToCard.vue, Product/AddToCardBtn.vue

---
+++ app/components/Product/AddToCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { Product } from "~~/types/product.types"
+
+defineProps<{ data: Product }>()
+const quantity = ref<number>(1)
+
+const updateQuantity = (value: number) => {
+  quantity.value = value
+}
+</script>
+
+<template>
+  <div class="add-to-cart">
+    <ProductCartCounter :on-add="updateQuantity" :on-remove="updateQuantity" />
+
+    <ProductAddToCartBtn :data="{ ...data, quantity }" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.add-to-cart {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 1rem;
+  background: #fff;
+  border-top: 1px solid rgba(0, 0, 0, 0.05);
+
+  @media (min-width: 768px) {
+    position: relative;
+    padding: 0;
+    border-top: none;
+    justify-content: center;
+  }
+}
+</style>
---
+++ app/components/Product/AddToCartBtn.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { computed } from "vue"
+
+import { useCartStore } from "~~/stores/cart"
+import { useProductsStore } from "~~/stores/products-cart"
+
+import type { Product } from "~~/types/product.types"
+
+const props = defineProps<{
+  data: Product & { quantity: number }
+}>()
+
+const cartStore = useCartStore()
+const productsStore = useProductsStore()
+
+const attributes = computed(() => [
+  productsStore.sizeSelection,
+  productsStore.colorSelection?.name,
+])
+
+const addToCartHandler = () => {
+  cartStore.addToCart({
+    id: props.data.id,
+    name: props.data.title,
+    srcUrl: props.data.srcUrl,
+    price: props.data.price,
+    attributes: attributes.value,
+    discount: props.data.discount,
+    quantity: props.data.quantity,
+  })
+}
+</script>
+
+<template>
+  <button type="button" class="add-to-cart-btn" @click="addToCartHandler">
+    Add to Cart
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.add-to-cart-btn {
+  width: 100%;
+  height: 2.75rem;
+  margin-left: 0.75rem;
+
+  background: #000;
+  color: #fff;
+  font-size: 0.875rem;
+  border-radius: 999px;
+
+  transition: background 0.2s ease;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.8);
+  }
+
+  @media (min-width: 640px) {
+    margin-left: 1.25rem;
+    font-size: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    height: 3.25rem;
+  }
+}
+</style>
