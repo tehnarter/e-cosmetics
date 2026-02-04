e6f087ed5a03b2487450fa1e9242ddaa556841d1
tehnarter <tehnarter@gmail.com>
2026-02-04 17:28:44 +0200

    feat(ui): add Separator.vue

---
+++ app/components/Ui/Separator.vue
@@ -0,0 +1,46 @@
+<script setup>
+const props = defineProps({
+  orientation: {
+    type: String,
+    default: "horizontal", // horizontal | vertical
+  },
+  decorative: {
+    type: Boolean,
+    default: true,
+  },
+  className: {
+    type: String,
+    default: "",
+  },
+})
+</script>
+
+<template>
+  <div
+    :aria-hidden="props.decorative"
+    :class="[
+      'separator',
+      props.orientation === 'horizontal'
+        ? 'separator--horizontal'
+        : 'separator--vertical',
+      props.className,
+    ]"
+  />
+</template>
+
+<style scoped lang="scss">
+.separator {
+  flex-shrink: 0;
+  background-color: #dcdcdc; // заміна bg-border
+}
+
+.separator--horizontal {
+  height: 1px;
+  width: 100%;
+}
+
+.separator--vertical {
+  width: 1px;
+  height: 100%;
+}
+</style>
